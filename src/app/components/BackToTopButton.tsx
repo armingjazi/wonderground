4b53d366b783f3adcaa7fa7eecbd17ebb33823d0
Armin Jazi <a.ghajarjazi@reply.de>
2025-03-16 09:51:03 +0100

    fix position of back to top

--- src/app/components/BackToTopButton.tsx
+++ src/app/components/BackToTopButton.tsx
@@ -4,7 +4,7 @@ export function BackToTopButton(props: { onClick: () => void }) {
   return (
     <motion.button
       onClick={props.onClick}
-      className="fixed bottom-12 right-1/2 z-50 text-3xl hover:opacity-70 transition-opacity bg-black/50 w-10 h-10 rounded-full flex items-center justify-center cursor-pointer"
+      className="fixed bottom-12 right-6 z-50 text-3xl hover:opacity-70 transition-opacity bg-black/50 w-10 h-10 rounded-full flex items-center justify-center cursor-pointer"
       initial={{ opacity: 0, scale: 0.8 }}
       animate={{ opacity: 1, scale: 1 }}
       transition={{ duration: 0.3, delay: 0.8 }}
