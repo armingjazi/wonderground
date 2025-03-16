9875eced6d9ff9aca08a490eb4ebaa61f84cedc6
Armin Jazi <a.ghajarjazi@reply.de>
2025-03-16 11:41:50 +0100

    fix back to top

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -2,6 +2,7 @@ import { AnimatePresence, motion } from "framer-motion";
 import { CloseButton } from "@/app/components/CloseButton";
 import { People } from "@/app/components/People";
 import { useScrollAnimation } from "@/app/util/useScrollAnimation";
+import { BackToTopButton } from "@/app/components/BackToTopButton";
 
 export const About = ({ onClose }: { onClose: () => void }) => {
   const root = useScrollAnimation(onClose);
@@ -26,25 +27,9 @@ export const About = ({ onClose }: { onClose: () => void }) => {
           <People />
 
           <CloseButton onClick={onClose} />
-          <motion.button
+          <BackToTopButton
             onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
-            className="fixed bottom-12 right-1/2 z-50 text-3xl hover:opacity-70 transition-opacity bg-black/50 w-10 h-10 rounded-full flex items-center justify-center cursor-pointer"
-            initial={{ opacity: 0, scale: 0.8 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 0.3, delay: 0.8 }}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="48"
-              height="48"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-            >
-              <polyline points="18 15 12 9 6 15"></polyline>
-            </svg>
-          </motion.button>
+          />
         </div>
       </motion.div>
     </AnimatePresence>
